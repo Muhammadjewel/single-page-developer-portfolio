850e809ab2b8b3f1fc61554226c05c6d25529534
Muhammadjavohir <msuratov@gmail.com>
2023-09-09 21:22:59 +0500

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,26 @@
+/* const THEME_TOGGLER_BUTTON = '.theme-toggler'; */
+
+function init () {
+  /* const elThemeTogglerButton = document.querySelector(THEME_TOGGLER_BUTTON);
+
+
+  // DOM handlers
+  function switchTheme () {
+    const elRoot = document.documentElement
+    let dataTheme = elRoot.getAttribute('data-theme');
+
+    let newTheme = (dataTheme === 'light') ? 'dark' : 'light';
+
+    elRoot.setAttribute('data-theme', newTheme);
+
+    localStorage.setItem('theme', newTheme);
+  }
+
+
+  // Event listeners
+  if (elThemeTogglerButton) {
+    elThemeTogglerButton.addEventListener('click', switchTheme);
+  } */
+}
+
+document.addEventListener('DOMContentLoaded', init);
